a39d253a36cb8a774ef0c5818d413039a9697aed
Alex Perkins <alex.perkins@edelman.com>
2014-04-09 11:47:38 -0400

    added attribution and slightly more sematic figure element

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -4,13 +4,19 @@ ini_set('display_errors', 'On');
 
 $query_string = substr( $_SERVER['QUERY_STRING'], 1 );
 //echo $query_string;
-$insert = <<<'EOD'
-<img id="sexy-ted" src="http://kotz.sexy/ted-lilith.jpg" width="389"/>
+$insert = <<<EOD
+<figure id="sexy-ted">
+	<img src="http://kotz.sexy/ted-lilith.jpg" width="389"/>
+	<figcaption><span class="artwork-title">IshTed</span>
+	by
+	<a href="http://perk.ee">Perkee</a></figcaption>
+</figure>
 <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
 <style>
 	body { padding-left: 400px; }
 	h1, h2, h3, h4, h5, h6 { font-family: 'Lobster'; }
-	#sexy-ted { position: fixed; left: 10px; top:10px; }
+	#sexy-ted { position: fixed; left: 10px; top:10px; margin:0;}
+	.artwork-title{font-style:italic}
 </style>
 EOD;
 
